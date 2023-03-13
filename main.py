0e150031663eaa197de6557ee6c196ff818e7fda
Sumedh6824 <127741895+Sumedh6824@users.noreply.github.com>
2023-03-13 16:35:24 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+
+
+a = input("Enter First Digit: ")
+b = input("Enter second Digit: ")
+
+print( " Addition:" ,int(a)+ int(b))
+print( " Subtraction:", int(a)- int(b))
+print(" Multiplication:", int(a)* int(b))
+print( " Division:", int(a)/ int(b))
